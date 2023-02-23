e3e4f1f43ed537e6204b16f84051d3b9e4e8f1fc
MyriadColors <pedrotorresgamedesign@gmail.com>
2023-02-22 21:20:24 -0300

    main.js updated with basic implementation

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+console.log("Type startGame() to begin.")
+
 function getComputerChoice() {
     min = Math.ceil(1);
     max = Math.floor(9);
@@ -13,23 +15,57 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection !== R || playerSelection !== P || playerSelection !== S) {
-        return "Write either rock paper or scissors"
-    } else {
         if (playerSelection === computerSelection) {
-            return "Draw"
+            return "D" // Draw
         } else if (playerSelection === "rock" && computerSelection==="paper") {
-            return "Computer";
+            return "C"; // Computer wins
         } else if (playerSelection === "paper" && computerSelection==="rock") {
-            return "Player";
+            return "P"; // Player wins
         } else if (playerSelection === "scissors" && computerSelection==="paper") {
-            return "Player";
+            return "P";
         } else if (playerSelection === "paper" && computerSelection==="scissors") {
-            return "Computer";
+            return "C";
         } else if (playerSelection === "scissors" && computerSelection==="rock") {
-            return "Computer";
+            return "C";
         } else if (playerSelection === "rock" && computerSelection==="scissors") {
-            return  "Player";
+            return  "P";
         }
-    }   
+}   
+
+
+function startGame() {
+    let rounds = 0;
+    let playerScore = 0;
+    let computerScore = 0;
+    let draw = 0;
+
+    player = prompt("Type either rock, paper or scissors").toLowerCase()
+    
+    while (rounds < 5) {
+        computer = getComputerChoice();
+        result = playRound(player, computer);
+
+        if (result == "P") {
+            playerScore++;
+            console.log("Player")
+        } else if (result == "C") {
+            computerScore++;
+            console.log("Computer")
+        } else if (result == "D") {
+            draw++;
+            console.log("Draw")
+        }
+
+        rounds++;
+    }
+
+    const scores = `Player Score: ${playerScore} \n Computer Score: ${computerScore} \n Draws: ${draw}`
+
+    if (playerScore > computerScore) {
+        return `Player won. \n the results were:\n ${scores} \n type startGame() in the console to play again.`
+    } else if (playerScore < computerScore) {
+        return `Computer won. \n the results were:\n ${scores} \n type startGame() in the console to play again.`
+    } else {
+        return `DRAW!. \n the results were:\n ${scores} \n type startGame() in the console to play again.`
+    }
 }
